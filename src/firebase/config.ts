891728c415afe8a01077419bbb67099281c79344
syifa reina <syifareina97@gmail.com>
2025-11-29 07:03:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7254438497-d0959",
+  "appId": "1:687679226120:web:b4567ec7915c5ddc166cbd",
+  "apiKey": "AIzaSyCLTtDbMH9kA7zUGzvwBTmSTq0S-64dum8",
+  "authDomain": "studio-7254438497-d0959.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "687679226120"
+};
